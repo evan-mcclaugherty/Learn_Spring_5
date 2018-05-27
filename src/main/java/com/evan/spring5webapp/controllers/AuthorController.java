a9b9f3c9dc06a0b1b30f9e12bcabefd038c53ee4
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2018-05-26 21:46:07 -0600

    Add author and book controllers and views.

---
+++ src/main/java/com/evan/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,21 @@
+package com.evan.spring5webapp.controllers;
+
+import com.evan.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", this.authorRepository.findAll());
+        return "authors";
+    }
+}
